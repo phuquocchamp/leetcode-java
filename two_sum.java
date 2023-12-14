df9843ade820111bc70dc36ff466a4d35e93aeb2
phuquocchamp <hoangtanphuquoc@gmail.com>
2023-12-14 16:32:46 +0700

    setup all files

---
+++ two_sum.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class two_sum {
+    public static void main(String[] args) {
+        System.out.println("Hoang Tan Phu Quoc");
+    }
+}
